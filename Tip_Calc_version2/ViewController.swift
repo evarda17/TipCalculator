9dab60fa54b4ca3a5bf798b3e7dd9cfd9efa94f0
Eliza Vardanyan <evarda17@gmail.com>
2020-09-04 17:05:12 -0400

    Initial Commit

---
+++ Tip_Calc_version2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip_Calc_version2
+//
+//  Created by Eliza Vardanyan on 9/4/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
