412dcabddb1f3f9e5c195c1f027d42165f9b8b52
Eliza Vardanyan <evarda17@gmail.com>
2020-09-05 03:15:10 -0400

    Final Settings feature added

---
+++ Tip_Calc_version2/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Tip_Calc_version2
+//
+//  Created by Eliza Vardanyan on 9/5/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
